d6ea560f831d7d77b52dbec9f8c9fc15ea8fc61b
Daniel Kirsch <danishkirel@gmail.com>
2009-11-09 15:33:30 +0100

    remember sample counts

--- app.rb
+++ app.rb
@@ -6,11 +6,18 @@ require 'my_classifiers'
 CLASSIFIER = Classifiers[ENV['classifier'] && ENV['classifier'].to_sym || :default]
 #CLASSIFIER = Classifiers[:dcelastic]
 
+counts = Hash.new { |h,k| h[k] = 0 }
+
+get '/' do
+  JSON :counts => counts
+end
+
 post '/train/:id' do
   begin
     CLASSIFIER.train params[:id].to_sym, request.body.read
   #rescue
   end
+  counts[params[:id].to_sym] += 1
   halt 200, JSON(:message => "Symbol was successfully trained.")
 end
 
